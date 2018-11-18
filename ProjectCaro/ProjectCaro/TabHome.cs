06c26d64fe71902827fd7f5e966ad4751aacc74b
duongtung99 <38658555+duongtung99@users.noreply.github.com>
2018-11-18 23:15:24 +0700

    fix

--- ProjectCaro/ProjectCaro/TabHome.cs
+++ ProjectCaro/ProjectCaro/TabHome.cs
@@ -21,19 +21,34 @@ namespace ProjectCaro
             SendCreateRoom(user_id);
         }
 
+
+        private void btnVao_Click(object sender, EventArgs e)
+        {
+            var input = Interaction.InputBox("Nhập số phòng : ", "Caro", "", -1, -1);
+            room_no = input;
+            SendJoinRoom(user_id, room_no);
+        }
+
+
+
         private void btnVaoNhanh_Click(object sender, EventArgs e)
         {
             SendQuickJoin(user_id);
         }
 
-        private void btnVao_Click(object sender, EventArgs e)
-        {
-            //room_no = txtSophong.Text;
-            //SendJoinRoom(user_id, room_no);
-            var input = Interaction.InputBox("Nhập số phòng : ", "Caro", "", -1, -1);
-            room_no = input;
-            SendJoinRoom(user_id, room_no);
-        }
+
+        //private void RemoveHint(object sender, EventArgs e)
+        //{
+        //    txtSophong.Text = "";
+        //}
+
+        //private void ShowHint(object sender, EventArgs e)
+        //{
+        //    if (string.IsNullOrWhiteSpace(txtSophong.Text))
+        //    {
+        //        txtSophong.Text = "Enter...";
+        //    }
+        //}
 
     }
 }
--- ProjectCaro/ProjectCaro/TabLogin.cs
+++ ProjectCaro/ProjectCaro/TabLogin.cs
@@ -27,7 +27,7 @@ namespace ProjectCaro
                 //Load home
                 lblUsername.Text = user_id;
                 //check login và chạy hàm load
-                processbartime.Start();
+                processbartime.Enabled = true;
                 SendUserOnline(user_id);
             }
             else
@@ -46,20 +46,20 @@ namespace ProjectCaro
         {
             //không cho hành động khi load form 
             processBar1.Visible = true;
-            //processBar1.Value = processBar1.Value + 50;
-            //if (processBar1.Value >= 199)
-            //{
-            //    //dừng thanh load
-            //    processbartime.Enabled = false;
-            //    //mở trang home
-            //    HomeLoad();
-            //    tabControl.SelectTab(Home);
-            //}
-            processBar1.Increment(1);
-            if (processBar1.Value == processBar1.Maximum)
+            processBar1.Value = processBar1.Value + 50;
+            if (processBar1.Value >= 199)
             {
+                //dừng thanh load
+                processbartime.Enabled = false;
+                //mở trang home
+                HomeLoad();
                 tabControl.SelectTab(Home);
             }
+            //processBar1.Increment(1);
+            //if (processBar1.Value == processBar1.Maximum)
+            //{
+            //    tabControl.SelectTab(Home);
+            //}
         }
 
 
